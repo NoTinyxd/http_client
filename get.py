31aea57de4f45d3e4fef00f87591a21bfda2dc84
NoTinyxd <160841329+NoTinyxd@users.noreply.github.com>
2026-07-13 18:05:34 +0500

    Added http and https both support

--- get.py
+++ get.py
@@ -2,28 +2,61 @@ import socket
 import ssl
 from urllib.parse import urlparse
 def get(url):
-    if not url.startswith("http://") and not url.startswith("https://"):
-        url = "https://"+url #for now supports only https
+    
     p = urlparse(url)
+    print(p.path)
+    if p.scheme=="https":
+        if p.port:
+            port=p.port
+        else:
+            port = 443
+        sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        sock.connect((p.hostname,port))
+        context = ssl.create_default_context()
+        connection = context.wrap_socket(sock,server_hostname=p.hostname)
+        path = p.path or "/"
+        request = (
+            f"GET {path} HTTP/1.1\r\n"
+            f"Host: {p.hostname}\r\n"
+            "Connection: close\r\n"
+            "\r\n"
+        )
+        response = b""
+        connection.sendall(request.encode())
+        while True:
+            data = connection.recv(4096)
+            if not data:
+                break
+            response = response+data
+        print(response.decode())
+        connection.close()
+    elif p.scheme=="http":
+        if p.port:
+            port = p.port
+        else:
+            port = 80
+        sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        sock.connect((p.hostname,port))
+        connection=sock
+        path = p.path or "/"
+        request = (
+            f"GET {path} HTTP/1.1\r\n"
+            f"Host: {p.hostname}\r\n"
+            "Connection: close\r\n"
+            "\r\n"
+        )
+        response = b""
+        connection.sendall(request.encode())
+        while True:
+            data = connection.recv(4096)
+            if not data:
+                break
+            response = response+data
+        print(response.decode())
+        connection.close()
+    elif p.scheme=="":
+        raise ValueError("No scheme provided, expected http or https")
+    else:
+        raise ValueError("Invalid or unexpected scheme")
 
-    sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    sock.connect((p.hostname,443))
-    context = ssl.create_default_context()
-
-    s = context.wrap_socket(sock,server_hostname=p.hostname)
-    request = (
-        f"GET / HTTP/1.1\r\n"
-        f"Host: {p.hostname}\r\n"
-        "Connection: close\r\n"
-        "\r\n"
-    )
-    s.sendall(request.encode())
-    response = b""
-    while True:
-        data=s.recv(4096)
-        if not data:
-            break
-        response = response+data
-    print(response.decode())
-    s.close()
-get("google.com") # Default to HTTPS if no protocol is specified (HTTP not yet supported).
+get("https://example.com/") 
